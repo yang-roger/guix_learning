b7d4216e7279105d2c7c2fe9f32ee8736632afbd
yang-roger <rongjian.yang@outlook.com>
2026-09-09 19:50:21 +0800

    _gx_display_driver_16bpp_block_move

--- common/src/gx_display_driver_16bpp_block_move.cpp
+++ common/src/gx_display_driver_16bpp_block_move.cpp
@@ -58,100 +58,85 @@
 /*    GUIX Internal Code                                                  */
 /*                                                                        */
 /**************************************************************************/
-void _gx_display_driver_16bpp_block_move(GX_DRAW_CONTEXT *context,
-                                         GX_RECTANGLE *block, INT xshift, INT yshift)
+void _gx_display_driver_16bpp_block_move(GX_DRAW_CONTEXT* context,
+                                         GX_RECTANGLE* block, INT xshift, INT yshift)
 {
-USHORT *pGet;
-USHORT *pPut;
-INT     width;
-INT     width_in_bytes;
-INT     y;
-INT     height;
-
 #ifdef GX_ENABLE_CANVAS_PARTIAL_FRAME_BUFFER
     if (context->canvas->status & GX_CANVAS_PARTIAL_FRAME_BUFFER)
     {
-        /* Not supported. */
-        return;
+        return; // Not supported.
     }
 #endif
+
+    USHORT* pGet;
+    USHORT* pPut;
+    INT     width;
+    INT     width_in_bytes;
+    INT     y;
+    INT     height;
 
     if (xshift)
     {
         if (xshift > 0)
         {
-            /* have to copy from left to right. */
-            pPut = (USHORT *)context->memory;
-            pPut += block->top * context->pitch;
-            pPut += block->left + xshift;
+            // have to copy from left to right.
 
-            pGet = (USHORT *)context->memory;
+            pGet = (USHORT*)context->memory;
             pGet += block->top * context->pitch;
             pGet += block->left;
 
-            width = block->right - block->left + 1 - xshift;
-            width_in_bytes = width * (int)sizeof(USHORT);
+            pPut = pGet + xshift;
 
-            if (width_in_bytes <= 0)
-            {
-                return;
-            }
-
-            for (y = block->top; y <= block->bottom; y++)
-            {
-                memmove(pPut, pGet, (size_t)width_in_bytes);
-
-                pPut += context->pitch;
-                pGet += context->pitch;
-            }
+            width = block->width_() - xshift;
         }
-        else
+        else // (x_shift < 0)
         {
-            /* have to copy from right to left */
-            pPut = (USHORT *)context->memory;
+            // have to copy from right to left
+
+            pPut = (USHORT*)context->memory;
             pPut += block->top * context->pitch;
             pPut += block->left;
 
-            pGet = (USHORT *)context->memory;
-            pGet += block->top * context->pitch;
-            pGet += block->left - xshift;
+            pGet = pPut - xshift;
 
-            width = block->right - block->left + 1 + xshift;
-            width_in_bytes = width * (int)sizeof(USHORT);
+            width = block->width_() + xshift;
+        }
 
-            if (width_in_bytes <= 0)
-            {
-                return;
-            }
+        if (width <= 0)
+        {
+            return;
+        }
 
-            for (y = block->top; y <= block->bottom; y++)
-            {
-                memmove(pPut, pGet, (size_t)width_in_bytes);
+        width_in_bytes = width * (int)sizeof(USHORT);
 
-                pPut += context->pitch;
-                pGet += context->pitch;
-            }
+        for (y = block->top; y <= block->bottom; ++y)
+        {
+            memmove(pPut, pGet, (size_t)width_in_bytes);
+
+            pPut += context->pitch;
+            pGet += context->pitch;
         }
     }
-    else
+    else if (yshift)
     {
-        width = block->right - block->left + 1;
+        width = block->width_();
         width_in_bytes = width * (int)sizeof(USHORT);
 
         if (yshift > 0)
         {
-            /* have to copy from top to bottom */
-            pPut = (USHORT *)context->memory;
+            // have to copy from top to bottom
+
+            pPut = (USHORT*)context->memory;
             pPut += block->bottom * context->pitch;
             pPut += block->left;
 
-            pGet = (USHORT *)context->memory;
+            pGet = (USHORT*)context->memory;
             pGet += (block->bottom - yshift) * context->pitch;
             pGet += block->left;
 
-            height = block->bottom - block->top + 1 - yshift;
+            height = block->height_() - yshift;
 
-            for (y = 0; y < height; y++)
+            for (y = 0; y < height; ++y)
             {
                 memmove(pPut, pGet, (size_t)width_in_bytes);
 
@@ -159,20 +144,21 @@ INT     height;
                 pGet -= context->pitch;
             }
         }
-        else
+        else // (yshift < 0)
         {
-            /* have to copy from bottom to top */
-            pPut = (USHORT *)context->memory;
+            // have to copy from bottom to top
+
+            pPut = (USHORT*)context->memory;
             pPut += block->top * context->pitch;
             pPut += block->left;
 
-            pGet = (USHORT *)context->memory;
+            pGet = (USHORT*)context->memory;
             pGet += (block->top - yshift) * context->pitch;
             pGet += block->left;
 
-            height = block->bottom - block->top + 1 + yshift;
+            height = block->height_() + yshift;
 
-            for (y = 0; y < height; y++)
+            for (y = 0; y < height; ++y)
             {
                 memmove(pPut, pGet, (size_t)width_in_bytes);
 
